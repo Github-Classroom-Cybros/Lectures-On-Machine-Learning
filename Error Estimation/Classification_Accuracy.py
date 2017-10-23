0f0416a951221ba99f116d13f300189b470cc841
Ayush Goel <ayush.goel501@gmail.com>
2017-10-23 13:22:17 +0530

    Error estimation
    Area under roc curve
    Classification_accuracy
    logarithmic loss

---
+++ Error Estimation/Classification_Accuracy.py
@@ -0,0 +1,16 @@
+# Cross Validation Classification Accuracy
+import pandas
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+seed = 7
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+model = LogisticRegression()
+scoring = 'accuracy'
+results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+print("Accuracy: %.3f (%.3f)") % (results.mean(), results.std())
